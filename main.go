d084d1ac117a1a5848ea8bc92d85fe99d6f7cc78
Alessio Greggi <ale_grey_91@hotmail.it>
2024-04-09 19:10:20 +0200

    refact: move to libbpfgo
    Signed-off-by: Alessio Greggi <ale_grey_91@hotmail.it>

--- main.go
+++ main.go
@@ -6,27 +6,28 @@ import (
 	"encoding/binary"
 	"flag"
 	"fmt"
-	"log"
 	"os"
 	"os/exec"
 	"os/signal"
 	"path"
 	"path/filepath"
-	"strings"
+	"unsafe"
 
-	"github.com/iovisor/gobpf/bcc"
+	"github.com/aquasecurity/libbpfgo"
+	bpf "github.com/aquasecurity/libbpfgo"
+	"github.com/aquasecurity/libbpfgo/helpers"
 )
 
 type event struct {
-	// syscall number
-	SyscallID uint32
-	// Stops tracing syscalls if true
+	SyscallID     uint32
 	TracingStatus uint32
 }
 
-//go:embed ebpf/*
-var eBPFDir embed.FS
+//go:embed output/*
+var eBPFObject embed.FS
 var version = "test"
+var tracepointCategory = "raw_syscalls"
+var tracepointName = "sys_enter"
 
 func main() {
 
@@ -35,8 +36,8 @@ func main() {
 	commandOutput := flag.Bool("co", false, "Print command output")
 	// define usage
 	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "\nUsage: %s [options] [command]\n", path.Base(os.Args[0]))
-		fmt.Printf("\nOptions:\n")
+		fmt.Printf("Usage: %s [options] [command]\n", path.Base(os.Args[0]))
+		fmt.Printf("Options:\n")
 		flag.PrintDefaults()
 		fmt.Printf("\nversion: %s\n", version)
 	}
@@ -44,13 +45,13 @@ func main() {
 	flag.Parse()
 
 	if !isRunningAsRoot() {
-		fmt.Println("Not enough privileges to run the program")
+		fmt.Println("not enough privileges to run the program")
 		os.Exit(1)
 	}
 
-	// Check if the -f flag is provided and has a value
+	// Check if the -fn flag is provided and has a value
 	if *functionName == "" {
-		fmt.Println("Error: -fn flag is mandatory. Please provide the name of the function to trace.")
+		fmt.Println("flag -fn is mandatory, please provide function name to trace.")
 		os.Exit(1)
 	}
 
@@ -58,46 +59,116 @@ func main() {
 	command := flag.Args()
 	// Check if there are any arguments after the -f flag
 	if len(command) == 0 {
-		fmt.Println("Error: Command to execute is mandatory. Please provide the command.")
+		fmt.Println("command argument is mandatory.")
 		os.Exit(1)
 	}
 
-	source, _ := eBPFDir.ReadFile("ebpf/ebpf.c")
-	src := strings.Replace(string(source), "$CMD", filepath.Base(command[0]), -1)
-	bpfModule := bcc.NewModule(src, []string{})
+	// suppress libbpf log ouput
+	libbpfgo.SetLoggerCbs(
+		bpf.Callbacks{
+			Log: func(level int, msg string) {
+				return
+			},
+		},
+	)
+
+	objectFile, err := eBPFObject.ReadFile("output/ebpf.o")
+	bpfModule, err := bpf.NewModuleFromBuffer(objectFile, "ebpf.o")
+	if err != nil {
+		fmt.Printf("error loading BPF object file: %v\n", err)
+		os.Exit(-1)
+	}
 	defer bpfModule.Close()
 
-	uprobeFd, err := bpfModule.LoadUprobe("enter_function")
+	/*
+		HashMap used for passing various configuration
+		from user-space to kernel-space.
+	*/
+	config, err := bpfModule.GetMap("config_map")
 	if err != nil {
-		log.Fatal(err)
-	}
-	uretprobeFd, err := bpfModule.LoadUprobe("exit_function")
-	if err != nil {
-		log.Fatal(err)
-	}
-	startTrace, err := bpfModule.LoadTracepoint("start_trace")
-	if err != nil {
-		log.Fatal(err)
+		fmt.Printf("error retrieving map from BPF program: %v\n", err)
+		os.Exit(-1)
 	}
 
-	err = bpfModule.AttachUprobe(command[0], *functionName, uprobeFd, -1)
+	bpfModule.BPFLoadObject()
+	enterFuncProbe, err := bpfModule.GetProgram("enter_function")
 	if err != nil {
-		log.Fatal(err)
-	}
-	err = bpfModule.AttachUretprobe(command[0], *functionName, uretprobeFd, -1)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if err := bpfModule.AttachTracepoint("raw_syscalls:sys_enter", startTrace); err != nil {
-		log.Fatal(err)
+		fmt.Printf("error loading program 'enter_function': %v\n", err)
+		os.Exit(-1)
 	}
 
-	table := bcc.NewTable(bpfModule.TableId("events"), bpfModule)
-	channel := make(chan []byte)
-
-	perfMap, err := bcc.InitPerfMap(table, channel, nil)
+	exitFuncProbe, err := bpfModule.GetProgram("exit_function")
 	if err != nil {
-		log.Fatal(err)
+		fmt.Printf("error loading program 'exit_function': %v\n", err)
+		os.Exit(-1)
+	}
+
+	traceFunction, err := bpfModule.GetProgram("start_trace")
+	if err != nil {
+		fmt.Printf("error loading program 'start_trace': %v\n", err)
+		os.Exit(-1)
+	}
+
+	offset, err := helpers.SymbolToOffset(command[0], *functionName)
+	if err != nil {
+		fmt.Printf("error finding %s function offset: %v\n", *functionName, err)
+		os.Exit(-1)
+	}
+	enterLink, err := enterFuncProbe.AttachUprobe(-1, command[0], offset)
+	if err != nil {
+		fmt.Printf("error attaching uprobe at function: %s, offset: %d\n", *functionName, offset)
+		os.Exit(-1)
+	}
+	defer enterLink.Destroy()
+
+	/*
+		Since the uretprobes doesn't work well with Go binaries,
+		we are going to attach a uprobe ∀ RET instruction withing the
+		traced function.
+	*/
+	exitLinks := make([]*bpf.BPFLink, 0)
+	functionRetOffsets, err := getFunctionRetOffsets(command[0], *functionName)
+	for _, offsetRet := range functionRetOffsets {
+		exitLink, err := exitFuncProbe.AttachUprobe(-1, command[0], offset+uint32(offsetRet))
+		exitLinks = append(exitLinks, exitLink)
+		if err != nil {
+			fmt.Printf("error attaching uprobe at function RET: %s, offset: %d", *functionName, offset+uint32(offsetRet))
+			os.Exit(-1)
+		}
+		defer func() {
+			for _, up := range exitLinks {
+				up.Destroy()
+			}
+			return
+		}()
+	}
+
+	traceLink, err := traceFunction.AttachTracepoint(tracepointCategory, tracepointName)
+	if err != nil {
+		fmt.Printf("error attaching tracepoint at event: %s:%s", tracepointCategory, tracepointName)
+		os.Exit(-1)
+	}
+	defer traceLink.Destroy()
+
+	/*
+		Sending input argument to BPF program
+		to instruct tracing specific command taken from cli.
+	*/
+	config_key_command := 0
+	baseCommand := filepath.Base(command[0])
+	baseCmd := append([]byte(baseCommand), 0)
+	err = config.Update(unsafe.Pointer(&config_key_command), unsafe.Pointer(&baseCmd[0]))
+	if err != nil {
+		fmt.Printf("error updating config_map with values: %d - '%s', %v\n", config_key_command, baseCommand, err)
+		os.Exit(-1)
+	}
+
+	eventsChannel := make(chan []byte)
+	lostChannel := make(chan uint64)
+	rb, err := bpfModule.InitPerfBuf("events", eventsChannel, lostChannel, 1024)
+	if err != nil {
+		fmt.Println("error initializing PerfBuffer: %v\n", err)
+		os.Exit(-1)
 	}
 
 	c := make(chan os.Signal, 1)
@@ -111,13 +182,12 @@ func main() {
 			fmt.Printf("%s\n", outErr)
 		}
 		defer cmd.Wait()
-		cmd.Stdin, cmd.Stdout, cmd.Stderr = os.Stdin, os.Stdout, os.Stderr
 	}()
 
 	var syscalls []uint32
-	executionStarted := false
+	executionIsStarted := false
 	go func() {
-		for data := range channel {
+		for data := range eventsChannel {
 			var e event
 			if err := binary.Read(bytes.NewBuffer(data), binary.LittleEndian, &e); err != nil {
 				fmt.Printf("failed to decode received data %q: %s\n", data, err)
@@ -125,18 +195,18 @@ func main() {
 			}
 			switch e.TracingStatus {
 			case 1:
-				fmt.Fprintln(os.Stdout, "[+] start tracing")
-				executionStarted = true
+				executionIsStarted = true
 			case 2:
-				fmt.Fprintln(os.Stdout, "[+] stop tracing")
-				executionStarted = false
+				executionIsStarted = false
 
 				// write to file or stdout depending on the flags passed
 				if *outputFile != "" {
 					file, _ := createFile(outputFile)
 					defer file.Close()
+					// write to file
 					printSyscalls(file, syscalls)
 				} else {
+					// write to stdout
 					printSyscalls(os.Stdout, syscalls)
 				}
 
@@ -144,14 +214,14 @@ func main() {
 				p, _ := os.FindProcess(os.Getpid())
 				p.Signal(os.Interrupt)
 			default:
-				if executionStarted {
+				if executionIsStarted {
 					syscalls = append(syscalls, e.SyscallID)
 				}
 			}
 		}
 	}()
 
-	perfMap.Start()
+	rb.Poll(300)
 	<-c
-	perfMap.Stop()
+	rb.Stop()
 }
--- utils.go
+++ utils.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"bytes"
+	"debug/elf"
 	"fmt"
 	"io"
 	"os"
 	"os/user"
 
 	seccomp "github.com/seccomp/libseccomp-golang"
+	"golang.org/x/arch/x86/x86asm"
 )
 
 // printSyscalls takes an io.Writer and slice of syscall ids,
@@ -42,4 +45,76 @@ func createFile(outputFile *string) (*os.File, error) {
 		return nil, fmt.Errorf("error: %v", err)
 	}
 	return file, nil
+}
+
+func getFunctionRetOffsets(elfFile string, fnName string) ([]uint64, error) {
+	// this code was taken from here:
+	// https://github.com/cfc4n/go_uretprobe_demo/blob/master/ret_offset.go#L22
+	var goSymbs []elf.Symbol
+	var goElf *elf.File
+	goElf, err := elf.Open(elfFile)
+	if err != nil {
+		return []uint64{}, err
+	}
+	goSymbs, err = goElf.Symbols()
+	if err != nil {
+		return nil, err
+	}
+
+	var found bool
+	var symbol elf.Symbol
+	for _, s := range goSymbs {
+		if s.Name == fnName {
+			symbol = s
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		return nil, fmt.Errorf("symbol not found")
+	}
+
+	section := goElf.Sections[symbol.Section]
+
+	var elfText []byte
+	elfText, err = section.Data()
+	if err != nil {
+		return nil, err
+	}
+
+	start := symbol.Value - section.Addr
+	end := start + symbol.Size
+
+	var instHex []byte
+	instHex = elfText[start:end]
+	var offsets []uint64
+	offsets, err = decodeInstruction(instHex)
+	if len(offsets) == 0 {
+		return offsets, fmt.Errorf("no RET instructions found")
+	}
+
+	return offsets, nil
+}
+
+func decodeInstruction(instHex []byte) ([]uint64, error) {
+	// this code was taken from here:
+	// https://github.com/cfc4n/go_uretprobe_demo/blob/master/ret_offset.go#L70C1-L91C2
+	var offsets []uint64
+	var s *bytes.Buffer
+	s = bytes.NewBufferString("")
+	for i := 0; i < len(instHex); {
+		inst, err := x86asm.Decode(instHex[i:], 64)
+		s.WriteString(fmt.Sprintf("%04X\t%s", i, inst.String()))
+		s.WriteString("\n")
+		if err != nil {
+			return nil, err
+		}
+		if inst.Op == x86asm.RET {
+			offsets = append(offsets, uint64(i))
+		}
+		i += inst.Len
+	}
+
+	return offsets, nil
 }
