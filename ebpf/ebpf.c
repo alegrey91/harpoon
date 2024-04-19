d7f6298b77dae82eb1e1ee3c83c7630ce51e2d07
Alessio Greggi <ale_grey_91@hotmail.it>
2024-04-19 10:37:36 +0200

    chore(ebpf/): add maps to send data more efficiently
    Signed-off-by: Alessio Greggi <ale_grey_91@hotmail.it>

--- ebpf/ebpf.c
+++ ebpf/ebpf.c
@@ -16,10 +16,20 @@ struct {
     __uint(value_size, sizeof(u32));
 } events SEC(".maps");
 
+struct {
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __uint(max_entries, 1);
+    __type(key, u32);
+    __type(value, struct tracing);
+} tracing_status SEC(".maps");
+
 // used to store the data received from the event
 struct syscall_data {
 	u32 syscall_id;
-	u32 tracing_status;
+};
+
+struct tracing {
+	u32 status;
 };
 
 // implement strncmp function
@@ -42,9 +52,11 @@ __bpf_strncmp(const void *x, const void *y, __u64 len) {
 // execution
 SEC("uprobe/enter_function")
 int enter_function(struct pt_regs *ctx) {
-	struct syscall_data data = {};
-	data.tracing_status = 1;
-	bpf_perf_event_output(ctx, &events, BPF_F_CURRENT_CPU, &data, sizeof(data));
+	struct tracing tc = {};
+	__u32 key_map_trace = 0;
+	tc.status = 1;
+	bpf_map_update_elem(&tracing_status, &key_map_trace, &tc, 0);
+	bpf_printk("enter function");
 	return 0;
 }
 
@@ -53,22 +65,36 @@ int enter_function(struct pt_regs *ctx) {
 // execution
 SEC("uprobe/exit_function")
 int exit_function(struct pt_regs *ctx) {
-	struct syscall_data data = {};
-	data.tracing_status = 2;
-	bpf_perf_event_output(ctx, &events, BPF_F_CURRENT_CPU, &data, sizeof(data));
+	struct tracing tc = {};
+	__u32 key_map_trace = 0;
+	tc.status = 2;
+	bpf_map_update_elem(&tracing_status, &key_map_trace, &tc, 0);
+	bpf_printk("exit function");
 	return 0;
 }
 
 SEC("tracepoint/raw_syscalls/sys_enter")
-int start_trace(struct trace_event_raw_sys_enter* args) {
+int trace_syscall(struct trace_event_raw_sys_enter* args) {
 	struct syscall_data data = {};
+	struct tracing *tc;
 	char comm[25];
-	__u32 key_map = 0;
+	__u32 key_map_config = 0;
+	__u32 key_map_trace = 0;
+
+	tc = bpf_map_lookup_elem(&tracing_status, &key_map_trace);
+	if (!tc) {
+		bpf_printk("error getting tracing status\n");
+		return 1;
+	}
+	if (tc->status != 1) {
+		//bpf_printk("tracing is not active, status=%d", tc->status);
+		return 1;
+	}
 
 	bpf_get_current_comm(&comm, sizeof(comm));
 
 	// lookup command passed as argument from Go application
-	char *input_command = bpf_map_lookup_elem(&config_map, &key_map);
+	char *input_command = bpf_map_lookup_elem(&config_map, &key_map_config);
 	if (!input_command) {
 		return 1;
 	}
@@ -86,6 +112,7 @@ int start_trace(struct trace_event_raw_sys_enter* args) {
 	int id = (int)args->id;
 	data.syscall_id = id;
 	bpf_perf_event_output(args, &events, BPF_F_CURRENT_CPU, &data, sizeof(data));
+	bpf_printk("sending syscall ID: %d", id);
 	return 0;
 }
 
